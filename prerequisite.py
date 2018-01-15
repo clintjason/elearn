bcd40fb1a9e22699a7e7f0087e8f5f079036321d
clintjason <ngassajason@yahoo.fr>
2018-01-15 12:15:43 +0100

    course prerequisite module

---
+++ prerequisite.py
@@ -0,0 +1,17 @@
+def Prerequisite():
+	prerequisites = [
+				{'Learn Bootstrap': ["HTML","HTML5","CSS","CSS3","JavaScript"]},
+				{'Learn Flask' : ["Python"]},
+				{'Learn JQuery': ["JavaScript"]},
+				{'Learn AJax' : ["JavaScript","JQuery"]},
+				{'Learn Angularjs' : ["HTML","CSS","JavaScript","MVC Concepts", "The DOM"]},
+				{'Learn Angular Material' : ["Angularjs"]},
+				{'Learn Angular2' : ["TypeScript","ES6","JavaScript","Object Oriented JavaScript"]},
+				{'Learn ASP.Net' : ["C","C++","CSharp","Visual Basic"]},
+				{'Learn Aurelia' : ["JavaScript","HTML","CSS"]},
+				{'Learn BackboneJS' : ["JavaScript"]},
+				{'Learn CakePHP' : ["PHP","HTML","CSS"]},
+				{'Learn Laravel' : ["PHP","HTML","CSS"]}
+
+	]
+	return prerequisites
