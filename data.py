cbc863c65f3bd02db490f9bddbb171620549386b
clintjason <ngassajason@yahoo.fr>
2018-01-15 12:16:23 +0100

    data module

---
+++ data.py
@@ -0,0 +1,76 @@
+def Courses():
+	courses = [
+		{
+			'id' : 1,
+			'title' : 'Learn AJax',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 2,
+			'title' : 'Learn Angularjs',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 3,
+			'title' : 'Learn Angular Material',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 4,
+			'title' : 'Learn Angular2',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 5,
+			'title' : 'Learn ASP.Net',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 6,
+			'title' : 'Learn Aurelia',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 7,
+			'title' : 'Learn BackboneJS',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 8,
+			'title' : 'Learn CakePHP',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 9,
+			'title' : 'Learn Bootstrap',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 10,
+			'title' : 'Learn JQuery',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 11,
+			'title' : 'Learn Laravel',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		},
+		{
+			'id' : 12,
+			'title' : 'Learn Flask',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'category' : 'Web Development'
+		}
+	]
+	return courses
